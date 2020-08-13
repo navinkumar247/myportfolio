275dd1b0515c941885462344a1e58ea76746759b
Navin <navinkumar247@gmail.com>
2020-08-13 14:31:01 +0530

    Updates

---
+++ precache-manifest.c76ecc9f94381a6b47124c83806af01d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fbb6769a19a64b823e3d43abc1abef",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "05ed4940bad377fd1ca6",
+    "url": "/myportfolio/static/css/2.d64ab0c6.chunk.css"
+  },
+  {
+    "revision": "8d53709d28fdfdc81013",
+    "url": "/myportfolio/static/css/main.88f9486d.chunk.css"
+  },
+  {
+    "revision": "05ed4940bad377fd1ca6",
+    "url": "/myportfolio/static/js/2.4aeacddb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfolio/static/js/2.4aeacddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d53709d28fdfdc81013",
+    "url": "/myportfolio/static/js/main.6892977e.chunk.js"
+  },
+  {
+    "revision": "2aecadd77da826673134",
+    "url": "/myportfolio/static/js/runtime-main.473ab83c.js"
+  },
+  {
+    "revision": "343bf7cf3756a972d73659d02ef6ab6d",
+    "url": "/myportfolio/static/media/Basava-site.343bf7cf.gif"
+  },
+  {
+    "revision": "15d6bd4cb4b783e734751aedfb53aa74",
+    "url": "/myportfolio/static/media/Facerecog-site.15d6bd4c.gif"
+  },
+  {
+    "revision": "21d620ef0ececdc58f216f04f20d915b",
+    "url": "/myportfolio/static/media/github.21d620ef.svg"
+  },
+  {
+    "revision": "047003b2f79f829cf76ae3e63841303b",
+    "url": "/myportfolio/static/media/linkedin.047003b2.svg"
+  },
+  {
+    "revision": "47cc7679cad8eb808bac57d150c4d4cc",
+    "url": "/myportfolio/static/media/me.47cc7679.jpg"
+  },
+  {
+    "revision": "27617320c3ffcf3b633012542a054bf3",
+    "url": "/myportfolio/static/media/shumvil-site.27617320.gif"
+  }
+]);
